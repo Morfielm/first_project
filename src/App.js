69abb421ff00e123d84bca524a40153591a0c0b5
Mariia Dvoriankina <morfielmm@gmail.com>
2023-05-28 22:05:16 +0200

    without jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{new Date().getFullYear()}</p>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    createApp();
+  }, []);
+
+  const createImg = () => {
+    const img = document.createElement("img");
+    img.src = logo;
+    img.className = "App-logo";
+    img.alt = "logo";
+
+    return img;
+  };
+
+  const createP = (value) => {
+    const p = document.createElement("p");
+    p.innerHTML = value;
+    
+    return p;
+  }
+
+  const createA = () => {
+    const a = document.createElement("a");
+    a.className = 'App-link';
+    a.href = 'https://reactjs.org';
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    a.innerHTML = 'Learn React';
+
+    return a;
+
+  }
+
+  const createApp = () => {
+    const container = document.createElement("div");
+    container.className = "App";
+    const header = document.createElement("header");
+    header.className = "App-header";
+
+    header.appendChild(createImg());
+    header.appendChild(createP('Edit <code>src/App.js</code> and save to reload.'));
+    header.appendChild(createA());
+    header.appendChild(createP(new Date().getFullYear().toString()));
+    container.appendChild(header);
+
+    document.body.replaceWith(container);
+  };
+
+  return null;
 }
 
 export default App;
